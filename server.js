c5da984878cfdf67b0c12557afc07e2a3350cd65
Justin Ramirez <therhelist@gmail.com>
2021-09-15 11:37:05 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 
 const mockUserData=[
 {name:'Mark'},
 {name:'Jill'}
 ]
+
 app.get('/users', function(req,res){
 	res.json({
 		success: true,
@@ -13,6 +17,7 @@ app.get('/users', function(req,res){
 	})
 })
 
+//colons are used as variables that can be viewed in the params
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
 	res.json({
@@ -22,5 +27,27 @@ app.get('/users/:id',function(req,res){
 	})
 })
 
+app.post('/login',function(req,res){
+	//Typically passwords are encrypted using something like bcrypt before sending to database
+ 	const username=req.body.username;
+ 	const password=req.body.password;
+ 
+	//This should come from database
+ 	const mockUsername="billyTheKid";
+ 	const mockPassword="superSecret";
+ 
+ 	if (username===mockUsername && password===mockPassword){
+      		res.json({
+      	 		success: true,
+      	 		message: 'password and username match!',
+      	 		token: 'encrypted token goes here'
+      		})
+ 	} else {
+      		res.json({
+      	 		success: false,
+      	 		message: 'password and username do not match'
+      		})
+ 	}
+})
 
 app.listen(8000,function(){console.log("server is listening")})
